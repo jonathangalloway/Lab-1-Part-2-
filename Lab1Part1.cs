db6012b45a5bcae783bcb44734ad7a4e843bb14e
jonathangalloway <mrjonathangalloway@yahoo.com>
2018-08-15 23:24:30 -0400

    Lab 1

---
+++ Lab1Part1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace WindowsFormsApp2
+{
+    public class Hello
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Please type in an integer");
+            string input1 = Console.ReadLine();
+            int num1;
+            int.TryParse(input1, out num1);
+
+            num1 = Convert.ToInt16(input1);
+
+            Console.WriteLine("Hello, Please type in an integer");
+            string input2 = Console.ReadLine();
+            int num2;
+            int.TryParse(input2, out num2);
+
+            num2 = Convert.ToInt16(input2);
+
+            int answer = num1 - num2
+
+
+         Console.WriteLine(answer);
+
+        }
+    }
+}
